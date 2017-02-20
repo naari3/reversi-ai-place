b1c2af048e0b3bc110ddab35783b958f1dcad2f0
naari3 <qazaqaza98@gmail.com>
2017-02-20 19:52:21 +0900

    test for reversi_board

---
+++ tests/test_board.py
@@ -0,0 +1,60 @@
+# -*- config: utf-8 -*-
+import sys
+sys.path.append('..')
+from models import ReversiBoard
+
+import pytest
+
+
+class TestBoard(object):
+
+    def text_moves(self):
+        board = ReversiBoard()
+        result = board.put_piece(26, 2)
+        assert result == 1
+        result = board.put_piece(18, 1)
+        assert result == 1
+        assert all(board.board == [
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1, 0, 0, 0, 0, 0,
+            0, 0, 2, 1, 2, 0, 0, 0,
+            0, 0, 0, 2, 1, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ])
+
+    def test_invalid_moves(self):
+        board = ReversiBoard()
+        with pytest.raises(Exception):
+            board.put_piece(1,1)
+
+        with pytest.raises(Exception):
+            board.put_piece(1,2)
+
+        assert all(board.board == [
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 1, 2, 0, 0, 0,
+            0, 0, 0, 2, 1, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ])
+
+    def test_export(self):
+        board = ReversiBoard()
+        data = board.export_board()
+        init_data = [
+            (0, 0, 0, 0, 0, 0, 0, 0),
+            (0, 0, 0, 0, 0, 0, 0, 0),
+            (0, 0, 0, 0, 0, 0, 0, 0),
+            (0, 0, 0, 1, 2, 0, 0, 0),
+            (0, 0, 0, 2, 1, 0, 0, 0),
+            (0, 0, 0, 0, 0, 0, 0, 0),
+            (0, 0, 0, 0, 0, 0, 0, 0),
+            (0, 0, 0, 0, 0, 0, 0, 0),
+        ]
+        assert data == init_data
