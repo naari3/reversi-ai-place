7f59ef6cbd652d891f24bff8fb9d8c69faba7eeb
naari3 <qazaqaza98@gmail.com>
2017-02-20 19:51:56 +0900

    handle for websocket

--- handles/board_ws.py
+++ handles/board_ws.py
@@ -6,19 +6,12 @@ class BoardWebSocketHandler(tornado.websocket.WebSocketHandler):
     boards = {}
 
     def open(self, borad_id):
+        print(borad_id)
         self.boards.get(borad_id, self)
-        print('Session {} opened by {}'.format(board_id, self.request.remote_ip))
+        # print('Session {} opened by {}'.format(board_id, self.request.remote_ip))
 
     def on_message(self, message):
-        message = json.loads(message)
-        t = Tokenizer()
-        tokens = t.tokenize(message["text"])
-        message["text"] = ""
-        for token in tokens:
-            message["text"] += "{} ".format(token.surface)
-        for user in self.users:
-            user.write_message(message)
+        self.write_message(message)
 
     def on_close(self):
-        self.users.remove(self)
         print('Session closed by {}'.format(self.request.remote_ip))
