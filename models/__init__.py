b2395ba67f4799133ffedf25e64c9d4112b18d87
naari3 <qazaqaza98@gmail.com>
2017-02-20 19:51:27 +0900

    put piece

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 from .reversi_board import *
+from .status import *
+from .board_gm import *
--- models/reversi_board.py
+++ models/reversi_board.py
@@ -5,7 +5,7 @@ class ReversiBoard(object):
     def __init__(self, resume_data=None):
         super(ReversiBoard, self).__init__()
         if resume_data:
-            self.board = self.resume_board()
+            self.board = self.resume_board(resume_data)
         else:
             self.board = self.create_board()
 
@@ -17,6 +17,37 @@ class ReversiBoard(object):
 
     def resume_board(self, resume_data):
         return create_board()
+
+    def put_piece(self, p, w, puton=True, chk=True, other_board=None):
+        if isinstance(p, list):
+            p = (p[0])+p[1]*8
+            if p > 63:
+                p = (p[0]-1)+(p[1]-1)*8
+        t, x, y = 0, p%8, p//8
+        for di, fi in zip([-1, 0, 1], [x, 7, 7-x]):
+            for dj, fj in zip([-8, 0, 8], [y, 7, 7-y]):
+                if not di == dj == 0:
+                    if other_board:
+                        b = other_board[p+di+dj::di+dj][:min(fi, fj)]
+                    else:
+                        b = self.board[p+di+dj::di+dj][:min(fi, fj)]
+                    n = (b==3-w).cumprod().sum()
+                    if b.size <= n or b[n] != w: n = 0
+                    t += n
+                    if puton:
+                        b[:n] = w
+        if puton:
+            if other_board:
+                if chk: assert(other_board[p] == 0 and t > 0)
+                other_board[p] = w
+            else:
+                if chk: assert(self.board[p] == 0 and t > 0)
+                self.board[p] = w
+        return t
+
+    def export_board(self):
+        data = list(zip(*[iter(self.board.tolist())]*8))
+        return data
 
     def print_board(self):
         print('  a b c d e f g h')
