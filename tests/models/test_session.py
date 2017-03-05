15207c16c943227274cc8ffd4b47a13d637d6741
naari3 <qazaqaza98@gmail.com>
2017-03-05 20:58:41 +0900

    update test

--- tests/models/test_session.py
+++ tests/models/test_session.py
@@ -14,56 +14,49 @@ dotenv_path = join(os.getcwd(), '.env')
 load_dotenv(dotenv_path)
 
 
-@pytest.fixture(autouse=True, scope='class')
-def redis_set():
-    return redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=os.environ.get("REDIS_PORT"))
-
-
-@pytest.mark.usefixtures('redis_set')
 class TestSession(object):
 
     @classmethod
     def setup_class(cls):
-        r = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=int(os.environ.get("REDIS_PORT")), password=os.environ.get("REDIS_PASS", None))
-        r.flushall()
-        ss = SessionStore(r, **dict(key_prefix='test'))
+        cls.redis = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=int(os.environ.get("REDIS_PORT")), password=os.environ.get("REDIS_PASS", None))
+        cls.redis.flushall()
 
-    def test_prefixed(self, redis_set):
-        ss = SessionStore(redis_set, **dict(key_prefix='test'))
+    def test_prefixed(self):
+        ss = SessionStore(self.redis, **dict(key_prefix='test'))
         prxd = ss.prefixed('testsid')
 
         assert prxd == 'test:testsid'
 
-    def test_gen_sid(self, redis_set):
-        ss = SessionStore(redis_set, **dict(key_prefix='test'))
+    def test_gen_sid(self):
+        ss = SessionStore(self.redis, **dict(key_prefix='test'))
         sid = ss.generate_sid()
 
         assert isinstance(sid, str)
 
-    def test_set_session(self, redis_set):
-        ss = SessionStore(redis_set, **dict(key_prefix='test', expire=0))
+    def test_set_session(self):
+        ss = SessionStore(self.redis, **dict(key_prefix='test', expire=0))
         ss.set_session('testsid', 'testname', 'testdata')
-        data = redis_set.hget('test:testsid', 'testname')
+        data = self.redis.hget('test:testsid', 'testname')
 
         assert data == b'testdata'
 
-    def test_set_session_with_expire(self, redis_set):
-        ss = SessionStore(redis_set, **dict(key_prefix='test', expire=1000))
+    def test_set_session_with_expire(self):
+        ss = SessionStore(self.redis, **dict(key_prefix='test', expire=1000))
         ss.set_session('testsid', 'testname', 'testdata')
-        data = redis_set.hget('test:testsid', 'testname')
+        data = self.redis.hget('test:testsid', 'testname')
 
         assert data == b'testdata'
-        assert redis_set.ttl('test:testsid')
+        assert self.redis.ttl('test:testsid')
 
-    def test_get_session(self, redis_set):
-        ss = SessionStore(redis_set, **dict(key_prefix='test'))
-        redis_set.hset('test:testsid', 'testname', 'testdata')
+    def test_get_session(self):
+        ss = SessionStore(self.redis, **dict(key_prefix='test'))
+        self.redis.hset('test:testsid', 'testname', 'testdata')
         data = ss.get_session('testsid', 'testname')
 
         assert 'testdata' == data
 
-    def test_delete_session(self, redis_set):
-        ss = SessionStore(redis_set, **dict(key_prefix='test'))
+    def test_delete_session(self):
+        ss = SessionStore(self.redis, **dict(key_prefix='test'))
         ss.delete_session('testsid')
 
-        assert redis_set.hgetall('testsid') == {}
+        assert self.redis.hgetall('testsid') == {}
