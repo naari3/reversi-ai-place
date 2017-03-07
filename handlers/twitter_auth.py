4b148f04e4bba5d69f56d1953bb26a62b7e64235
naari3 <qazaqaza98@gmail.com>
2017-03-07 09:39:55 +0900

    redirect to 'next'

--- handlers/twitter_auth.py
+++ handlers/twitter_auth.py
@@ -11,22 +11,20 @@ from models import User
 
 
 class AuthHandler(BaseHandler, tornado.auth.TwitterMixin):
-    @tornado.web.asynchronous
-    @gen.engine
+    @gen.coroutine
     def get(self):
         if self.get_argument("oauth_token", None):
-            user = yield gen.Task(self.get_authenticated_user)
-            print(user)
-            # user = yield self.get_authenticated_user()
+            user = yield self.get_authenticated_user()
             if not user:
-                raise tornado.web.HTTPError(500, "Twitter auth failed")
+                return tornado.web.HTTPError(500, "Twitter auth failed")
 
-            # exist = bool(list(User.select().where(User.twitter_id == user['id'])))
             reversi_user = User.get_or_create(twitter_id=user['id'])[0]
             reversi_user.name = user['name']
             reversi_user.save()
 
             self.save_current_user(reversi_user)
             self.redirect(self.get_argument("next", "/"))
+        else:
+            yield self.authorize_redirect(callback_uri=self.request.full_url())
 
         self.authenticate_redirect()
