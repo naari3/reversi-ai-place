d6a337c3b90552a077cb534b9749408492f3606f
naari3 <qazaqaza98@gmail.com>
2017-03-10 05:46:56 +0900

    add OAuthAccessToken refresh

--- handlers/oauth_mock.py
+++ handlers/oauth_mock.py
@@ -15,3 +15,25 @@ class OAuthVerify(BaseHandler):
 
         self.set_header("Content-Type", "application/json")
         self.write(json.dumps(data))
+
+
+class OAuthAccessToken(BaseHandler):
+
+    def post(self):
+        handle_type = self.get_argument("grant_type")
+        if handle_type == 'refresh_token':
+            refresh_token = self.get_argument("refresh_token")
+            client_id = self.get_argument("client_id")
+            client_secret = self.get_argument("client_secret")
+
+            access_token = AccessToken(self.application.access_token_store).refresh(refresh_token)
+
+            data = {
+                'token_type': 'Bearer',
+                'access_token': access_token.access_token,
+                'expires_in': access_token.access_token_store.options.expire,
+                'refresh_token': access_token.refresh_token,
+            }
+
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps(data))
