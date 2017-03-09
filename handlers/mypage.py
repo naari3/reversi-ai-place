47a861f2279a23fc30769cce264e7d188c4a03d3
naari3 <qazaqaza98@gmail.com>
2017-03-10 05:53:45 +0900

    add handler for mypage

--- handlers/mypage.py
+++ handlers/mypage.py
@@ -10,13 +10,18 @@ class MyPageHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         user = self.get_current_user()
+        data = {
+            "user": user,
+            "access_token": None,
+        }
+        self.render("mypage.pug", **data)
+
+    @tornado.web.authenticated
+    def post(self):
+        user = self.get_current_user()
         access_token = AccessToken(self.application.access_token_store, user_id=user.id)
         data = {
             "user": user,
             "access_token": access_token,
         }
         self.render("mypage.pug", **data)
-
-    @tornado.web.authenticated
-    def post(self):
-        pass
