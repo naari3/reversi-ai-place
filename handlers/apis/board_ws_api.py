12c9646ff4f57a4412f44a79dfd486e382f4dffc
naari3 <qazaqaza98@gmail.com>
2017-03-12 18:40:36 +0900

    closing and delete

--- handlers/apis/board_ws_api.py
+++ handlers/apis/board_ws_api.py
@@ -22,21 +22,35 @@ class BoardWebSocketAPIHandler(BaseAPIWebSocketHandler):
             'ws': self,
             'user': self.get_token_user(),
         }
-        print(player['user'].twitter_id)
+        print(player['user'].id)
         if not self.application.boards[board_id].add_player(player):
             self.close(code=1003, reason="You can't join to [{board_id}]")
 
     def on_close(self):
         board_id = self.application.board_id_dict[self]
         print(f"closed and disconnected to {board_id} by {self.request.remote_ip}")
-        self.application.boards[board_id].remove_player(self)
+        if self.application.boards.get(board_id, None):
+            self.application.boards[board_id].close_board()
+            del self.application.boards[board_id]
 
     def on_message(self, message):
         board_id = self.application.board_id_dict[self]
         input_data = json.loads(message)
-        if input_data.get('x', False) and input_data.get('y', False):
-            self.application.boards[board_id].receive_move(
-                self, input_data['x'], input_data['y'])
-        else:
-            self.write_message(json.dumps(
-                {meta: {'status': 400, 'message': 'illegal move'}}))
+        try:
+            if input_data.get('x', False) and input_data.get('y', False):
+                self.application.boards[board_id].receive_move(
+                    self, input_data['x'], input_data['y'])
+            else:
+                data = self.application.boards[board_id].extract_data()
+                data['meta'] = {
+                    'status': 400,
+                    'message': 'illegal move'
+                }
+                self.write_message(json.dumps(data))
+        except Exception as e:
+            data = self.application.boards[board_id].extract_data()
+            data['meta'] = {
+                'status': 400,
+                'message': 'illegal move'
+            }
+            self.write_message(json.dumps(data))
