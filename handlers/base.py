48a86881d950353b82c3146a9f9d96759b4eef8d
naari3 <qazaqaza98@gmail.com>
2017-04-02 10:23:04 +0900

    add base hander for websocket

--- handlers/base.py
+++ handlers/base.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
 import tornado.escape
 import tornado.web
+import tornado.websocket
 
 from models import User, Session
 
 
 class BaseHandler(tornado.web.RequestHandler):
+
+    def get_current_user(self):
+        sid = self.get_secure_cookie("sid")
+        if not sid:
+            return None
+        sid = sid.decode('utf-8')
+        session = Session(self.application.session_store, sid)
+        if not session.data:
+            return None
+        if session.data.get('twitter_id'):
+            user = User.get(twitter_id=session.data['twitter_id'])
+        else:
+            return None
+
+        return user
+        # return tornado.escape.json_decode(sid)
+
+    def save_current_user(self, user):
+        session = Session(self.application.session_store)
+        session.data = user.fields()
+        session.save()
+        self.set_secure_cookie("sid", session.session_id)
+
+    def destroy_current_user(self, user):
+        self.set_secure_cookie("sid", '')
+
+
+class BaseWebSocketHandler(tornado.web.RequestHandler):
 
     def get_current_user(self):
         sid = self.get_secure_cookie("sid")
